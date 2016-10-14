fe8eebce9a79368969fbf052f767b3c30c36b3eb
Stavros Korokithakis <stavros@korokithakis.net>
2016-10-14 21:35:45 +0300

    Add the "own" variable if the user has the right conversation key.

--- main/views/generic.py
+++ main/views/generic.py
@@ -2,6 +2,7 @@ from annoying.decorators import render_to
 from django.db.models import Max, Count
 from django.conf import settings
 from django.shortcuts import get_object_or_404
+from django.utils.crypto import constant_time_compare
 
 from ..models import Conversation
 
@@ -20,4 +21,5 @@ def home(request):
 @render_to("conversation_view.html")
 def conversation_view(request, conversation_id):
     conversation = get_object_or_404(Conversation, pk=conversation_id)
-    return {"conversation": conversation}
+    own_conversation = constant_time_compare(request.GET.get("key"), conversation.secret_key)
+    return {"conversation": conversation, "own": own_conversation}
