075c1d0af74ba25ccd8f6b817b617c574f4de827
Stavros Korokithakis <stavros@korokithakis.net>
2016-11-21 16:46:23 +0200

    Add admin link.

---
+++ classification/smoke_tests.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+
+class SmokeTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('user', 'user@user.com', 'password')
+        self.user.is_staff = True
+        self.user.save()
+
+    def test_urls(self):
+        response = self.client.get(reverse("classification:classify"))
+        self.assertEqual(response.status_code, 404)
+
+        self.client.login(username="user", password="password")
+
+        response = self.client.get(reverse("classification:classify"))
+        self.assertEqual(response.status_code, 200)
