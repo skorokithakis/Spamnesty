61c463b0b30c3881950324821d807a556cd7e02a
Stavros Korokithakis <stavros@korokithakis.net>
2016-11-23 21:30:46 +0200

    Fix typo.

--- main/views/webhooks.py
+++ main/views/webhooks.py
@@ -69,7 +69,7 @@ def email(request):
     message = Message.parse_from_mailgun(request.POST)
 
     # If there is no unsent message in the queue, queue one.
-    if not message.converation.messages.exclude(send_on=None).exists():
+    if not message.conversation.messages.exclude(send_on=None).exists():
         # Reply to the spammer.
         reply = construct_reply(message)
         reply.queue()
