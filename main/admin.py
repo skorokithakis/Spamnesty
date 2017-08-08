6fb78cf304984c600416da3681636c36338879a7
Stavros Korokithakis <hi@stavros.io>
2017-08-08 20:20:13 +0300

    Fix wrong fields

--- main/admin.py
+++ main/admin.py
@@ -30,7 +30,7 @@ class MessageInline(admin.TabularInline):
 @admin.register(Conversation)
 class ConversationAdmin(admin.ModelAdmin):
     list_display = ["id", "sender_name", "sender_email", "domain", "category"]
-    search_fields = ["id", "profile"]
+    search_fields = ["id", "sender_email", "sender_name"]
     list_filter = ["category"]
     inlines = [MessageInline]
 
